1eee9834d9274157b436696bdf8f0cd0a366cff0
Ronan Barzic <rbarzic@gmail.com>
2015-02-02 19:01:29 +0100

    Remove dependency to cider-nrepl

--- insta-simd/project.clj
+++ insta-simd/project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [instaparse "1.3.2"]
+                 [instaparse "1.3.5"]
                  [rhizome "0.2.1"]]
   :profiles {:uberjar {:aot :all}
-             :dev {:plugins [[com.jakemccrary/lein-test-refresh "0.5.1"] [cider/cider-nrepl "0.7.0"]]}}
+             :dev {:plugins [[com.jakemccrary/lein-test-refresh "0.5.1"]]}}
 )
